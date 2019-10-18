68f47f94246a94618781baad94bac29815fb3bee
baxle <50596760+baxle@users.noreply.github.com>
2019-10-18 22:31:11 +0300

    Добавлен метод addImage. Добавлен String.format, добавлен предикат в методе findText.

--- src/main/java/interfaces/ExcelReader.java
+++ src/main/java/interfaces/ExcelReader.java
@@ -1,10 +1,16 @@
 package interfaces;
+
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
+
 import java.io.*;
 import java.net.URL;
+import java.util.function.BiPredicate;
+
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.util.WorkbookUtil;
+import org.apache.poi.util.IOUtils;
+
 
 public class ExcelReader implements CanDo {
 
@@ -12,22 +18,29 @@ public class ExcelReader implements CanDo {
     private int textCount;
     private final Logger logger = Logger.getLogger(ExcelReader.class);
     private int listCount;
+    private String fileName;
+
 
     public ExcelReader(String fileName) {
 
+        this.fileName = fileName;
+
         ClassLoader classLoader = getClass().getClassLoader();
         URL resource = classLoader.getResource(fileName);
+        System.out.println("filePath " + resource);
+
 
         if (resource == null) {
-            logger.error("Заданного файла " + fileName + " не существует.");
-            throw new IllegalArgumentException("Заданного файла " + fileName + " не существует.");
-        }
-        else {
+            String s = String.format("Заданного файла " + fileName + " не существует.");
+            logger.error(s);
+            throw new IllegalArgumentException(s);
+        } else {
+
             try {
                 workbook = WorkbookFactory.create(new File(resource.getFile()));
             } catch (FileNotFoundException e) {
-                System.err.printf("Файла %s не существует.\n", fileName);
-                logger.error("Заданного файла " + fileName + " не существует.");
+                String s = String.format("Заданного файла " + fileName + " не существует.");
+                logger.error(s);
                 System.exit(0);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -37,7 +50,6 @@ public class ExcelReader implements CanDo {
                 logger.error("Ошибка InvalidFormatException.");
             }
         }
-
     }
 
 
@@ -55,11 +67,11 @@ public class ExcelReader implements CanDo {
             sheet.forEach(row -> {
                 row.forEach(cell -> {
                     if (cell.getCellTypeEnum() == CellType.STRING) {
-                        if (cell.getStringCellValue().contains(text) && !equalsOrContains) {
-                            System.out.printf("Текст %s найден в листе %s в ячейке %s.\n", text, cell.getSheet().getSheetName(), cell.getAddress());
-                            textCount++;
-                        } else if (cell.getStringCellValue().equals(text) && equalsOrContains) {
-                            System.out.printf("Текст %s найден в листе %s в ячейке %s.\n", text, cell.getSheet().getSheetName(), cell.getAddress());
+                        BiPredicate<String, String> predicate = equalsOrContains ? String::equals : String::contains;
+                        if (predicate.test(cell.toString(), text)) {
+                            // System.out.printf("Текст %s найден в листе %s в ячейке %s.\n", text, cell.getSheet().getSheetName(), cell.getAddress());
+                            String s = String.format("Текст %s найден в листе %s в ячейке %s.", text, cell.getSheet().getSheetName(), cell.getAddress());
+                            System.out.println(s);
                             textCount++;
                         }
                     }
@@ -68,10 +80,11 @@ public class ExcelReader implements CanDo {
         });
 
         if (textCount != 0) {
-            logger.info("Искомый текст " + text + " встречается в файле " + textCount + " раз(а).");
+            String s = String.format("Искомый текст " + text + " встречается в файле " + textCount + " раз(а).");
+            logger.info(s);
         } else if (textCount == 0) {
-            System.err.printf("Текст %s не найден.\n", text);
-            logger.info("Искомого текста " + text + " в файле не найдено.");
+            String s = String.format("Искомого текста " + text + " в файле не найдено.");
+            logger.info(s);
         }
         try {
             workbook.close();
@@ -93,8 +106,8 @@ public class ExcelReader implements CanDo {
 
         System.out.println(listCount);
         if (listCount == 0) {
-            System.err.printf("Листа %s не найдено. Создаем новый лист\n", sheetName);
-            logger.info("Листа " + sheetName + " в файле не найдено. Создаем такой лист.");
+            String s = String.format("Листа %s не найдено. Создаем новый лист", sheetName);
+            logger.info(s);
 
 
             String safeName = WorkbookUtil.createSafeSheetName(sheetName);
@@ -114,4 +127,131 @@ public class ExcelReader implements CanDo {
         }
     }
 
+    private void createList(String sheetName) {
+        listCount = 0;
+
+        workbook.forEach(sheet -> {
+            if (sheet.getSheetName().equals(sheetName)) {
+                listCount++;
+            }
+        });
+
+        if (listCount == 0) {
+            String s = String.format("Листа %s не найдено. Создаем новый лист", sheetName);
+            logger.info(s);
+            workbook.createSheet(sheetName);
+        }
+
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream(new File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            workbook.write(fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void addImage(String sheetName, String imageName) {
+
+        createList(sheetName);
+
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(imageName);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(new File(resource.getFile()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        byte[] bytes = new byte[0];
+        try {
+            bytes = IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Adds a picture to the workbook
+        int pictureIdx = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+        //close the input stream
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Returns an object that handles instantiating concrete classes
+        CreationHelper helper = workbook.getCreationHelper();
+        //Creates the top-level drawing patriarch.
+
+        final Drawing[] drawing = new Drawing[1];
+        workbook.forEach(sheet -> {
+            if (sheet.getSheetName().equals(sheetName)) {
+              drawing[0] = sheet.createDrawingPatriarch();
+            }
+        });
+
+
+        //Create an anchor that is attached to the worksheet
+        ClientAnchor anchor = helper.createClientAnchor();
+
+        //create an anchor with upper left cell _and_ bottom right cell
+        anchor.setCol1(1); //Column B
+        anchor.setRow1(2); //Row 3
+        anchor.setCol2(2); //Column C
+        anchor.setRow2(3); //Row 4
+
+        //Creates a picture
+        Picture pict = drawing[0].createPicture(anchor, pictureIdx);
+
+        //Reset the image to the original size
+        //pict.resize(); //don't do that. Let the anchor resize the image!
+
+        //Create the Cell B3
+        workbook.forEach(sheet -> {
+            if (sheet.getSheetName().equals(sheetName)) {
+                Cell cell = sheet.createRow(2).createCell(1);
+            }
+        });
+
+
+        //set width to n character widths = count characters * 256
+        //int widthUnits = 20*256;
+        //sheet.setColumnWidth(1, widthUnits);
+
+        //set height to n points in twips = n * 20
+        //short heightUnits = 60*20;
+        //cell.getRow().setHeight(heightUnits);
+
+        //Write the Excel file
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream(new File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            workbook.write(fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
+
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,19 +6,25 @@ import interfaces.ExcelReader;
 public class Main {
 
     private static final String fileName = "test1.xls";
-    private static final String cellText = "Евро";
-    private static final String imageName = "default.png";
-    private static final String sheetName = "List22";
+    private static final String cellText = "Евро2525";
+    private static final String imageName = "test.jpg";
+    private static final String sheetName = "List144";
     private static final String cellName = "A1";
-    private static final boolean equalsOrContains = false;
+    private static final boolean equalsOrContains = true;
 
 
     public static void main(String[] args) {
         CanDo excelReader = new ExcelReader(fileName);
+        ExcelReader excelReader1 = new ExcelReader(fileName);
 
         excelReader.findText(cellText, equalsOrContains);
         excelReader.findText("Дзю", false);
         // excelReader.insertImage(fileName, imageName, sheetName, cellName);
+
+     excelReader1.addImage(sheetName, imageName);
+
+
+
 
     }
 }
